37383067100d4d502e578c209f6cb1cc044942ec
lasarinii <pedroluislasarini23@gmail.com>
2023-04-12 14:24:26 -0300

    teste login

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,28 +1,45 @@
 <script lang="ts">
-     let theme: string
+     let theme: string;
 
-     $: if (typeof document !== 'undefined') {
-          if (theme != '') {
-               document.documentElement.setAttribute('data-theme', theme)
+     $: if (typeof document !== "undefined") {
+          if (theme != "") {
+               document.documentElement.setAttribute("data-theme", theme);
           } else {
-               document.documentElement.setAttribute('data-theme', 'default')
+               document.documentElement.setAttribute("data-theme", "default");
           }
      }
 </script>
 
-<nav class="navbar bg-base-100 fixed z-50 font-extrabold shadow">
+<nav class="navbar bg-base-100 fixed z-50 font-extrabold shadow p-0">
      <div class="navbar-start">
           <div class="dropdown">
                <label tabindex="0" class="btn btn-ghost lg:hidden">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    <svg
+                         xmlns="http://www.w3.org/2000/svg"
+                         class="h-5 w-5"
+                         fill="none"
+                         viewBox="0 0 24 24"
+                         stroke="currentColor"
+                         ><path
+                              stroke-linecap="round"
+                              stroke-linejoin="round"
+                              stroke-width="2"
+                              d="M4 6h16M4 12h8m-8 6h16"
+                         /></svg
+                    >
                </label>
-               <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+               <ul
+                    tabindex="0"
+                    class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+               >
                     <li><a href="#home">Home</a></li>
                     <li><a href="#sobre">Sobre</a></li>
                     <li><a href="#footer">Contato</a></li>
                </ul>
           </div>
-          <a class="btn btn-ghost normal-case text-2xl italic" href="/">Lasarini</a>
+          <a class="btn btn-ghost normal-case text-2xl italic" href="/"
+               >Lasarini</a
+          >
      </div>
      <div class="navbar-center hidden lg:flex">
           <ul class="menu menu-horizontal px-1">
@@ -33,22 +50,94 @@
      </div>
      <div class="navbar-end">
           <ul class="dropdown">
-                    <span tabindex="0" class="btn btn-ghost">
-                         <p class="capitalize text-md font-extrabold">Theme</p>
-                         <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
-                    </span>
-                    <ul class="-translate-x-8 p-3 menu menu-compact dropdown-content shadow bg-base-100 rounded-box" tabindex="0">
-                         <li><button on:click={() => { theme = 'light' }}>Light</button></li>
-                         <li><button on:click={() => { theme = 'dark' }}>Dark</button></li>
-                         <li><button on:click={() => { theme = 'retro' }}>Retro</button></li>
-                         <li><button on:click={() => { theme = 'valentine' }}>Valentine</button></li>
-                         <li><button on:click={() => { theme = 'aqua' }}>Aqua</button></li>
-                         <li><button on:click={() => { theme = 'lemonade' }}>Lemonade</button></li>
-                         <li><button on:click={() => { theme = 'coffee' }}>Coffee</button></li>
-                         <li><button on:click={() => { theme = 'luxury' }}>Luxury</button></li>
-                         <li><button on:click={() => { theme = 'synthwave' }}>Synthwave</button></li>
-                         <li><button on:click={() => { theme = 'default' }}>None</button></li>
-                    </ul>
+               <span tabindex="0" class="btn btn-ghost">
+                    <p class="capitalize text-md font-extrabold">Theme</p>
+                    <svg
+                         class="fill-current"
+                         xmlns="http://www.w3.org/2000/svg"
+                         width="20"
+                         height="20"
+                         viewBox="0 0 24 24"
+                         ><path
+                              d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"
+                         /></svg
+                    >
+               </span>
+               <ul
+                    class="-translate-x-8 p-3 menu menu-compact dropdown-content shadow bg-base-100 rounded-box"
+                    tabindex="0"
+               >
+                    <li>
+                         <button
+                              on:click={() => {
+                                   theme = "light";
+                              }}>Light</button
+                         >
+                    </li>
+                    <li>
+                         <button
+                              on:click={() => {
+                                   theme = "dark";
+                              }}>Dark</button
+                         >
+                    </li>
+                    <li>
+                         <button
+                              on:click={() => {
+                                   theme = "retro";
+                              }}>Retro</button
+                         >
+                    </li>
+                    <li>
+                         <button
+                              on:click={() => {
+                                   theme = "valentine";
+                              }}>Valentine</button
+                         >
+                    </li>
+                    <li>
+                         <button
+                              on:click={() => {
+                                   theme = "aqua";
+                              }}>Aqua</button
+                         >
+                    </li>
+                    <li>
+                         <button
+                              on:click={() => {
+                                   theme = "lemonade";
+                              }}>Lemonade</button
+                         >
+                    </li>
+                    <li>
+                         <button
+                              on:click={() => {
+                                   theme = "coffee";
+                              }}>Coffee</button
+                         >
+                    </li>
+                    <li>
+                         <button
+                              on:click={() => {
+                                   theme = "luxury";
+                              }}>Luxury</button
+                         >
+                    </li>
+                    <li>
+                         <button
+                              on:click={() => {
+                                   theme = "synthwave";
+                              }}>Synthwave</button
+                         >
+                    </li>
+                    <li>
+                         <button
+                              on:click={() => {
+                                   theme = "default";
+                              }}>None</button
+                         >
+                    </li>
+               </ul>
           </ul>
      </div>
 </nav>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,34 +1,43 @@
 <script lang="ts">
-     let dot: string
+     let dot: string;
 
      function addDot() {
-          if (typeof dot == 'undefined') {
-               dot = ' '
+          if (typeof dot == "undefined") {
+               dot = " ";
           }
-          if (dot == ' ...') {
-               dot = ' '
+          if (dot == " ...") {
+               dot = " ";
           } else {
-               dot = dot + '.'
+               dot = dot + ".";
           }
      }
 
-     setInterval(addDot, 800)
+     setInterval(addDot, 800);
 </script>
 
 <section id="home" class="hero min-h-screen bg-base-200">
      <div class="hero-content text-left">
           <div class="max-w-md">
                <p class="p-1 text-xl font-bold">Pedro Lasarini,</p>
-               <h1 class="text-5xl py-2 font-extrabold bg-clip-text text-transparent bg-gradient-to-br from-primary-focus to-secondary-focus">Estudante de Programação</h1>
-               <p class="py-6">Com 20 anos, curso Engenharia de Computação na <abbr title="Universidade Virtual do Estado de São Paulo">Univesp</abbr> e tenho amplo interesse na área de Tecnologia da Informação.</p>
+               <h1
+                    class="text-5xl py-2 font-extrabold bg-clip-text text-transparent bg-gradient-to-br from-primary-focus to-secondary-focus"
+               >
+                    Estudante de Programação
+               </h1>
+               <p class="py-6">
+                    Com 20 anos, curso Engenharia de Computação na <abbr
+                         title="Universidade Virtual do Estado de São Paulo"
+                         >Univesp</abbr
+                    > e tenho amplo interesse na área de Tecnologia da Informação.
+               </p>
           </div>
      </div>
 </section>
 
-<section id="sobre" class="hero min-h-screen">
+<section id="sobre" class="hero min-h-screen bg-base-200">
      <div class="hero-content text-center">
           <div class="max-w-md">
-               <h1 class="text-3xl font-bold">Under development{ dot }</h1>
+               <h1 class="text-3xl font-bold">Under development{dot}</h1>
           </div>
      </div>
 </section>
